1e53838e679ef430c0d66a350c5c8f103a3a4f4c
thienkk25 <thiennguyendz25@gmail.com>
2025-12-07 08:24:22 +0700

    refactor

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,8 @@
 import { Router } from 'express'
 import { authController } from '../controllers/auth.controller.js'
-import { validateMiddleware } from '../middlewares/validation.middleware.js'
-import { authValidationSchema } from '../validations/auth.validation.js'
 const router = Router()
 
-router.post('/login', validateMiddleware(authValidationSchema.signInWithPassword), authController.login)
+router.post('/login', authController.login)
 router.post('/register', authController.register)
 router.post('/logout', authController.signOut)
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express"
 import { authMiddleware } from '../middlewares/auth.middleware.js'
-import { validateMiddleware } from '../middlewares/validation.middleware.js'
-import { userValidationSchema } from '../validations/user.validation.js'
 import { userController } from '../controllers/user.controller.js'
 const router = Router()
 
 router.patch('/change-password', authMiddleware, userController.changePassword)
-router.patch('/update-phone', validateMiddleware(userValidationSchema.updatePhoneUser), authMiddleware, userController.updatePhone)
-router.patch('/update-fullname', validateMiddleware(userValidationSchema.updateFullNameUser), authMiddleware, userController.updateFullName)
+router.patch('/update-phone', authMiddleware, userController.updatePhone)
+router.patch('/update-fullname', authMiddleware, userController.updateFullName)
 
 export default router
