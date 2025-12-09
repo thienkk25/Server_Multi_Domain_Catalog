12468ea188e4adee7f5e33920f89f85050ff9b62
thienkk25 <thiennguyendz25@gmail.com>
2025-12-09 18:46:34 +0700

    fix

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { authController } from '../controllers/auth.controller.js'
+import { authMiddleware } from '../middlewares/auth.middleware.js'
+import { checkRole } from '../middlewares/role.middleware.js'
 const router = Router()
 
 router.post('/login', authController.login)
-router.post('/register', authController.register)
+router.post('/register', authMiddleware, checkRole(['admin']), authController.register)
 router.post('/logout', authController.signOut)
 
 export default router
