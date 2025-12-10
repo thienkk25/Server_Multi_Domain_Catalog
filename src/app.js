74e5195702fa9b6ef0e7e52b644895e9f78cd75c
thienkk25 <thiennguyendz25@gmail.com>
2025-12-10 13:41:01 +0700

    docs

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`)
+    next()
+})
+
 if (swaggerDocument) {
     app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 }
