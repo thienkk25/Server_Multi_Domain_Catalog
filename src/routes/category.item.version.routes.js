04bfa3cc74ecf5db1329e36637d28181f2951161
thienkk25 <thiennguyendz25@gmail.com>
2026-02-04 15:24:43 +0700

    fix

--- src/routes/category.item.version.routes.js
+++ src/routes/category.item.version.routes.js
@@ -11,12 +11,12 @@ router.get('/:id', categoryItemVersionController.getById)
 
 // domain officer
 router.post('/', authMiddleware, checkRole(['domainOfficer']), categoryItemVersionController.createVersion)
-router.post('/:id', authMiddleware, checkRole(['domainOfficer']), categoryItemVersionController.updateVersion)
-router.post('/:id', authMiddleware, checkRole(['domainOfficer']), categoryItemVersionController.deleteVersion)
+router.post('/:id/update', authMiddleware, checkRole(['domainOfficer']), categoryItemVersionController.updateVersion)
+router.post('/:id/delete', authMiddleware, checkRole(['domainOfficer']), categoryItemVersionController.deleteVersion)
 
 // approver
-router.post('/:id', authMiddleware, checkRole(['approver']), categoryItemVersionController.approveVersion)
-router.post('/:id', authMiddleware, checkRole(['approver']), categoryItemVersionController.rejectVersion)
+router.post('/:id/approve', authMiddleware, checkRole(['approver']), categoryItemVersionController.approveVersion)
+router.post('/:id/reject', authMiddleware, checkRole(['approver']), categoryItemVersionController.rejectVersion)
 
 // admin
 router.delete('/:id', authMiddleware, checkRole(['admin']), categoryItemVersionController.remove)
