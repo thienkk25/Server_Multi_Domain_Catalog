095fe5ac31c92f6e634f56b16795611de1b8e343
thienkk25 <thiennguyendz25@gmail.com>
2026-05-17 15:45:01 +0700

    feat: parse query parameters using qs in catalog controller methods

--- src/controllers/catalog.controller.js
+++ src/controllers/catalog.controller.js
@@ -1,14 +1,16 @@
 import { catalogService } from '../services/catalog.service.js'
 import { handle } from '../utils/handle.helper.js'
+import qs from 'qs'
+
 export const catalogController = {
-    getDomains: handle(req => catalogService.getDomains(req.query, req.apiKey)),
+    getDomains: handle(req => catalogService.getDomains(qs.parse(req.query), req.apiKey)),
     getDomainById: handle(req => catalogService.getDomainById(req.params.id, req.apiKey)),
 
-    getCategoryGroups: handle(req => catalogService.getCategoryGroups(req.query, req.apiKey)),
+    getCategoryGroups: handle(req => catalogService.getCategoryGroups(qs.parse(req.query), req.apiKey)),
     getCategoryGroupById: handle(req => catalogService.getCategoryGroupById(req.params.id, req.apiKey)),
 
-    getCategoryItems: handle(req => catalogService.getCategoryItems(req.query, req.apiKey)),
+    getCategoryItems: handle(req => catalogService.getCategoryItems(qs.parse(req.query), req.apiKey)),
     getCategoryItemById: handle(req => catalogService.getCategoryItemById(req.params.id, req.apiKey)),
 
-    syncCategoryItems: handle(req => catalogService.syncCategoryItems(req.query, req.apiKey)),
+    syncCategoryItems: handle(req => catalogService.syncCategoryItems(qs.parse(req.query), req.apiKey)),
 }
